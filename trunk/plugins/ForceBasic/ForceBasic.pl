6f4611ad11d6d5cbf58426a9014d1acffe76edaf
Chad Everett <github@jayseae.cxliv.org>
2009-07-17 11:04:00 -0400

    Update license to Artistic - 0.2.1

--- trunk/plugins/ForceBasic/ForceBasic.pl
+++ trunk/plugins/ForceBasic/ForceBasic.pl
@@ -1,5 +1,17 @@
 # ===========================================================================
-# Copyright Everitz Consulting.  Not for redistribution.
+# A Movable Type plugin to force limited authors to basic entry interface.
+# Copyright 2005 Everitz Consulting <everitz.com>.
+#
+# This program is free software:  You may redistribute it and/or modify it
+# it under the terms of the Artistic License version 2 as published by the
+# Open Source Initiative.
+#
+# This program is distributed in the hope that it will be useful but does
+# NOT INCLUDE ANY WARRANTY; Without even the implied warranty of FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+# You should have received a copy of the Artistic License with this program.
+# If not, see <http://www.opensource.org/licenses/artistic-license-2.0.php>.
 # ===========================================================================
 package MT::Plugin::ForceBasic;
 
@@ -13,8 +25,8 @@ MT->add_plugin($ForceBasic = __PACKAGE__->new({
   name => 'MT-ForceBasic',
   description => 'Force limited authors to basic entry interface.',
   author_name => 'Everitz Consulting',
-  author_link => 'http://www.everitz.com/',
-  version => '0.2.0'
+  author_link => 'http://everitz.com/',
+  version => '0.2.1'
 }));
 
 # callback registration
